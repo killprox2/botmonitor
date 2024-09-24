4af820de28e995882beb9b521092a4e7559d51e4
killprox2 <43413557+killprox2@users.noreply.github.com>
2024-09-24 09:05:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -722,7 +722,9 @@ async function checkManomanoDeals() {
       await sendLogMessage('⚠️ Erreur lors de la recherche des deals Manomano.');
       console.error('Erreur lors de la recherche des deals Manomano:', error);
   }
-    function rotateUserAgent() {
+
+}
+function rotateUserAgent() {
     const userAgents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
@@ -733,6 +735,3 @@ async function checkManomanoDeals() {
     ];
     return userAgents[Math.floor(Math.random() * userAgents.length)];
 }
-
-}
-
